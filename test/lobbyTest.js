37ef435819bc58938585dd94c69beaedd4683706
Aniket Kudtarkar <aniketkud22@gmail.com>
2022-11-28 10:00:25 +0530

    Test2

--- test/lobbyTest.js
+++ test/lobbyTest.js
@@ -20,77 +20,77 @@ let token
 
 describe('Air Hockey Test', ()=>{
 
-    // describe('Test1: Chat functionality',()=>{
-    //     it('Should be able to send and recieve message', (done)=>{
-    //         const client1 = io.connect(socketUrl)
-    //         client1.on('connect', ()=>{
-    //             client1.emit('new-user-joined', (userId1) )
-    //             const client2 = io.connect(socketUrl,options)
-    //             client2.on('connect',()=>{
-    //                 client2.emit('new-user-joined', (userId2) )
-    //                 client1.emit('send-message', "Test string")
-    //                 client2.on('receive-message', (res)=>{
-    //                     chai.expect(res.message).to.equal('Test string')
-    //                     done()
-    //                 })
-    //             })
-    //         })
-    //     })
-    // })
+    describe('Test1: Chat functionality',()=>{
+        it('Should be able to send and recieve message', (done)=>{
+            const client1 = io.connect(socketUrl)
+            client1.on('connect', ()=>{
+                client1.emit('new-user-joined', (userId1) )
+                const client2 = io.connect(socketUrl,options)
+                client2.on('connect',()=>{
+                    client2.emit('new-user-joined', (userId2) )
+                    client1.emit('send-message', "Test string")
+                    client2.on('receive-message', (res)=>{
+                        chai.expect(res.message).to.equal('Test string')
+                        done()
+                    })
+                })
+            })
+        })
+    })
 
     // user already exists
-    // describe('Test2: Registration',()=>{
-    //     it('Should return User already exists', (done)=>{
-    //         let credentials = {
-    //             username: "aniket",
-    //             password: "abcd"
-    //         }
-    //         chai.request(server)
-    //             .post('/test/register')
-    //             .send(credentials)
-    //             .end((err,resp)=>{
-    //                 resp.should.have.status(403)
-    //                 resp.text.should.be.eq('User already exists')
-    //             done()
-    //             })
-    //     })
-    // })
+    describe('Test2: Registration',()=>{
+        it('Should return User already exists', (done)=>{
+            let credentials = {
+                username: "aniket",
+                password: "abcd"
+            }
+            chai.request(server)
+                .post('/test/register')
+                .send(credentials)
+                .end((err,resp)=>{
+                    resp.should.have.status(403)
+                    resp.text.should.be.eq('User already exists')
+                done()
+                })
+        })
+    })
 
     // non existent user
-    // describe('Test3: Login Username',()=>{
-    //     it('Should return Cant find user', (done)=>{
-    //         let credentials = {
-    //             username: "ramesh",
-    //             password: "abcd"
-    //         }
-    //         chai.request(server)
-    //             .post('/test/login')
-    //             .send(credentials)
-    //             .end((err,resp)=>{
-    //                 resp.should.have.status(404)
-    //                 resp.text.should.be.eq('Cant find user')
-    //             done()
-    //             })
-    //     })
-    // })
+    describe('Test3: Login Username',()=>{
+        it('Should return Cant find user', (done)=>{
+            let credentials = {
+                username: "ramesh",
+                password: "abcd"
+            }
+            chai.request(server)
+                .post('/test/login')
+                .send(credentials)
+                .end((err,resp)=>{
+                    resp.should.have.status(404)
+                    resp.text.should.be.eq('Cant find user')
+                done()
+                })
+        })
+    })
 
-    //  // incorrect password
-    //  describe('Test4: Login Password',()=>{
-    //     it('Should return Incorrect Password', (done)=>{
-    //         let credentials = {
-    //             username: "aniket",
-    //             password: "abcd2"
-    //         }
-    //         chai.request(server)
-    //             .post('/test/login')
-    //             .send(credentials)
-    //             .end((err,resp)=>{
-    //                 resp.should.have.status(401)
-    //                 resp.text.should.be.eq('Incorrect Password')
-    //             done()
-    //             })
-    //     })
-    // })
+     // incorrect password
+     describe('Test4: Login Password',()=>{
+        it('Should return Incorrect Password', (done)=>{
+            let credentials = {
+                username: "aniket",
+                password: "abcd2"
+            }
+            chai.request(server)
+                .post('/test/login')
+                .send(credentials)
+                .end((err,resp)=>{
+                    resp.should.have.status(401)
+                    resp.text.should.be.eq('Incorrect Password')
+                done()
+                })
+        })
+    })
 
     // unauthorized
     describe('Test5: Unauthorized access',()=>{
@@ -105,73 +105,73 @@ describe('Air Hockey Test', ()=>{
         })
     })
 
-    // // succesful login
-    // describe('Test6: Successful access',()=>{
-    //     it('Should return authorization token', (done)=>{
-    //         let credentials = {
-    //             username: "aniket",
-    //             password: "abcd"
-    //         }
-    //         chai.request(server)
-    //             .post('/test/login')
-    //             .send(credentials)
-    //             .end((err,resp)=>{
-    //                 resp.should.have.status(200)
-    //                 resp.body.should.be.a('object')
-    //                 resp.body.should.have.property('signedToken')
-    //                 resp.body.should.have.property('expiresIn')
-    //                 token = resp.body.signedToken
-    //             done()
-    //             })
-    //     })
-    // })
+    // succesful login
+    describe('Test6: Successful access',()=>{
+        it('Should return authorization token', (done)=>{
+            let credentials = {
+                username: "aniket",
+                password: "abcd"
+            }
+            chai.request(server)
+                .post('/test/login')
+                .send(credentials)
+                .end((err,resp)=>{
+                    resp.should.have.status(200)
+                    resp.body.should.be.a('object')
+                    resp.body.should.have.property('signedToken')
+                    resp.body.should.have.property('expiresIn')
+                    token = resp.body.signedToken
+                done()
+                })
+        })
+    })
 
-    // // return username
-    // describe('Test7: Username on successful login',()=>{
-    //     it('Should return correct username', (done)=>{
-    //         chai.request(server)
-    //             .get('/test/lobby')
-    //             .set('Cookie', 'jwt='+token)
-    //             .end((err,resp)=>{
-    //                 resp.should.have.status(200)
-    //                 resp.text.should.be.eq('aniket')
-    //             done()
-    //             })
-    //     })
-    // })
+    // return username
+    describe('Test7: Username on successful login',()=>{
+        it('Should return correct username', (done)=>{
+            chai.request(server)
+                .get('/test/lobby')
+                .set('Cookie', 'jwt='+token)
+                .end((err,resp)=>{
+                    resp.should.have.status(200)
+                    resp.text.should.be.eq('aniket')
+                done()
+                })
+        })
+    })
 
-    // // Leaderboard test
-    // describe('Test8: Leaderboard',()=>{
-    //     it('Should return leaderboard array', (done)=>{
-    //         chai.request(server)
-    //             .get('/test/leaderboard')
-    //             .end((err,resp)=>{
-    //                 resp.should.have.status(200)
-    //                 resp.body.should.be.a('array')
-    //                 resp.body.length.should.be.eq(5)
-    //             done()
-    //             })
-    //     })
-    // })
+    // Leaderboard test
+    describe('Test8: Leaderboard',()=>{
+        it('Should return leaderboard array', (done)=>{
+            chai.request(server)
+                .get('/test/leaderboard')
+                .end((err,resp)=>{
+                    resp.should.have.status(200)
+                    resp.body.should.be.a('array')
+                    resp.body.length.should.be.eq(5)
+                done()
+                })
+        })
+    })
 
-    // // Personal Stats Test
-    // describe('Test9: Personal Stats',()=>{
-    //     it('Should return personal stats object', (done)=>{
-    //         chai.request(server)
-    //             .get('/test/personalStats')
-    //             .set('Cookie', 'jwt='+token)
-    //             .end((err,resp)=>{
-    //                 resp.should.have.status(200)
-    //                 resp.body.should.be.a('object')
-    //                 resp.body.should.have.property('username')
-    //                 resp.body.should.have.property('games')
-    //                 resp.body.should.have.property('wins')
-    //                 resp.body.should.have.property('loss')
-    //                 resp.body.should.have.property('points')
-    //             done()
-    //             })
-    //     })
-    // })    
+    // Personal Stats Test
+    describe('Test9: Personal Stats',()=>{
+        it('Should return personal stats object', (done)=>{
+            chai.request(server)
+                .get('/test/personalStats')
+                .set('Cookie', 'jwt='+token)
+                .end((err,resp)=>{
+                    resp.should.have.status(200)
+                    resp.body.should.be.a('object')
+                    resp.body.should.have.property('username')
+                    resp.body.should.have.property('games')
+                    resp.body.should.have.property('wins')
+                    resp.body.should.have.property('loss')
+                    resp.body.should.have.property('points')
+                done()
+                })
+        })
+    })    
 
     // Game Test
     describe('Test10: Game',()=>{
